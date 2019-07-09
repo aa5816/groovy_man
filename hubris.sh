84efa3c1e4d46a768dd69469ec03200640b61a1a
Anmol Ahuja <mvsquarer@gmail.com>
2019-07-09 17:19:37 +0530

    maps basics

--- hubris.sh
+++ hubris.sh
@@ -28,3 +28,12 @@ def roman = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII'] //creating a list
 assert roman[4] == 'IV'
 roman[8] = 'VIII' //dynamically increase the size of list
 assert roman.size() == 9
+
+def http = [
+100 : 'CONTINUE',
+200 : 'OK',
+400 : 'BAD REQUEST'
+]
+assert http[200] == 'OK'
+http[500] = 'INTERNAL SERVER ERROR'
+assert http.size() == 4
