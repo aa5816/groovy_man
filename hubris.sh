871fa412db08ea2dc24afd276407b15438d8790c
Anmol Ahuja <mvsquarer@gmail.com>
2019-07-09 18:00:09 +0530

    closures not so close

--- hubris.sh
+++ hubris.sh
@@ -46,3 +46,13 @@ assert o.from == 1
 assert o.to == 10
 assert o.reverse() == 10..1
 assert o.contains (10)
+
+[1, 2, 3].each { entry -> println entry } //prints all the elements of the list
+
+//find the number of clinks if a party with 100 guests and everyone clinks glasses with everybody else
+def totalClinks = 0
+def partyPeople = 100
+1.upto(partyPeople) { guestNumber -> clinksWithGuest = guestNumber-1
+totalClinks += clinksWithGuest
+}
+assert totalClinks == (partyPeople * (partyPeople-1)) / 2
