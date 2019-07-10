87057518f34676cec8862c5a3dc12d2567e0bc1a
Anmol Ahuja <mvsquarer@gmail.com>
2019-07-10 13:00:30 +0530

    over here riding

--- datatypes_stuff/override.groovy
+++ datatypes_stuff/override.groovy
@@ -1,7 +1,7 @@
 import groovy.transform.Immutable
 
-@Immutable class Money {
- 
+@Immutable class Money { //every immutable object automatically gets a value-based implementation of equals   
+
 int amount         //data memebers
 String currency
 
@@ -16,7 +16,7 @@ return new Money(amount + other.amount, currency)
 
 Money buck = new Money(1, 'USD')
 assert buck
-assert buck == new Money(1, 'USD')
+assert buck == new Money(1, 'USD') //Uses overridden ==
 assert buck + buck == new Money(2, 'USD') //asserts that our operator is overriden
 Money cash = new Money (2, 'INR')
 // buck + cash ,this will through an IllegalArgumentException with the message can't add INR TO USD 
