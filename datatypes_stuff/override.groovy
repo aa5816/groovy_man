7d6dad6eb214f4c77b0222bd4fc05c6ba8620074
Anmol Ahuja <mvsquarer@gmail.com>
2019-07-10 13:01:55 +0530

    over here riding

--- datatypes_stuff/override.groovy
+++ datatypes_stuff/override.groovy
@@ -5,7 +5,7 @@ import groovy.transform.Immutable
 int amount         //data memebers
 String currency
 
-Money plus (Money other) {     //overriding the plus method (+ operator)
+Money plus (Money other) {     //implementing the plus method (+ operator)
 if (null == other) return this
 if (other.currency != currency) {
 throw new IllegalArgumentException("cannot add $other.currency to $currency")
@@ -17,6 +17,6 @@ return new Money(amount + other.amount, currency)
 Money buck = new Money(1, 'USD')
 assert buck
 assert buck == new Money(1, 'USD') //Uses overridden ==
-assert buck + buck == new Money(2, 'USD') //asserts that our operator is overriden
+assert buck + buck == new Money(2, 'USD') //asserts that our operator is implemented
 Money cash = new Money (2, 'INR')
 // buck + cash ,this will through an IllegalArgumentException with the message can't add INR TO USD 
